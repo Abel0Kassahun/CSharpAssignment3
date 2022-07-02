c5a4742d20fec362bd40b0fa686c56d31308c314
Abel0Kassahun <abellakasuuju6624@gmail.com>
2022-07-02 16:37:09 +0300

    First commit to the 3rd lab assignment

---
+++ ShapeAreaPerimeter/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ShapeAreaPerimeter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle();
+            Console.WriteLine("The area of the circle is "+circle.area());
+            Console.WriteLine("The circumference of the circle is " + circle.perimeter());
+
+            Rectangle rec = new Rectangle(8, 9);
+            Console.WriteLine("The area of the rectangle is " + rec.area());
+            Console.WriteLine("The perimeter of the rectangle is " + rec.perimeter());
+        }
+    }
+    public interface IShape //Shape interface
+    {
+        double area();
+        double perimeter();
+    }
+    
+    public class Circle : IShape //Circle class
+    {
+        public double radius=12;
+        const double PI = 3.14;
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public Circle() {}
+        public double area()
+        {
+            double area = PI * (radius * radius);
+            return area;
+        }
+        public double perimeter()
+        {
+            double cicumference = 2 * (PI * radius);
+            return cicumference;
+        }
+    }
+    public class Rectangle :IShape //Rectangle class
+    {
+        public double length=5;
+        public double width=6;
+        public Rectangle(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public Rectangle() {}
+
+        public double area()
+        {
+            double area = length * width;
+            return area;
+        }
+        public double perimeter()
+        {
+            double perimeter = 2 * (length+width);
+             return perimeter;
+        }
+    }
+}
